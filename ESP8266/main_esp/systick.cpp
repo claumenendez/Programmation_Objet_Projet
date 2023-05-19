988ce15086c0f3fe0b843d5fd023ef2eac09efca
claumenendez <98877212+claumenendez@users.noreply.github.com>
2023-05-19 17:07:33 +0200

    Delete ESP8266/main_esp directory

--- ESP8266/main_esp/systick.cpp
+++
@@ -1,9 +0,0 @@
-#include "systick.hpp"
-
-void Systick::start_sys(unsigned long interval, void (*callback)()) {
-  ticker.attach(interval/ 1000.0, callback);  // interval to seconds
-}
-
-void Temporizador::stop_sys() {
-  ticker.detach();
-}
